90ab69f8d7ca041400e90e10580c8dc40b265686
Tomi933 <strashivskaoksana@gmail.com>
2024-09-24 18:26:14 +0300

    Другий коміт

--- Project1/project.cpp
+++ Project1/project.cpp
@@ -9,17 +9,19 @@ int main()
 	double y; 
 	double z1;
 	double z2;
+	double sum;
 
 	cout << "x = "; cin >> x;
 	cout << "y = "; cin >> y;
 
 	z1 = pow(cos(x), 4) + pow(sin(y), 2) + 1.0/4 * pow(sin(2 * x), 2) - 1;
 	z2 = sin(y + x) * sin(y - x);
-	
+	sum = z1 + z2;
 
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
+	cout << "sum = " << sum << endl;
 	cin.get();
 	return 0;
 	
